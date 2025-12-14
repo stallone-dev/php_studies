3f1131fae466eeb95f8cba026825835aefc2d3fa
stallone-dev <stallone.developer@gmail.com>
2025-12-14 19:37:30 -0300

    basic

---
+++ src/vanilla/ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX001 - PHP</title>
+</head>
+<body>
+    <header>
+        <h1>EX000 - PHP</h1>
+    </header>
+    <main>
+        <?php
+            $lang = "PHP \u{1F30E}";
+            echo "Olá $lang";
+        ?>
+    </main>
+</body>
+</html>
