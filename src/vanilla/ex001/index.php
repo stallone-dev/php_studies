b11efcc081d456093301c48807659e947dc5c4c4
stallone-dev <stallone.developer@gmail.com>
2025-12-14 19:37:30 -0300

    phpinfo

---
+++ src/vanilla/ex001/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX001 - PHP</title>
+</head>
+<body>
+    <header>
+        <h1>EX001 - PHP</h1>
+    </header>
+    <main>
+        <?php
+            phpinfo();
+        ?>
+    </main>
+</body>
+</html>
